ebf0e461071f310cf813e60a798950fc83569681
rodrigoarimochi <133044458+rodrigoarimochi@users.noreply.github.com>
2026-08-27 16:22:46 -0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.jsx"; // ✅ MUDANÇA AQUI: 'app.jsx' em minúsculo
+import App from "./App.jsx"; // ✅ Correto (A maiúsculo)
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
